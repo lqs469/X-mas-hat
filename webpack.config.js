239a49e8f63a6fa19bc956aaa5c9c1da6683e26b
lqs469 <liqinshuo469@gmail.com>
2019-12-25 17:00:11 +0800

    a

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production',
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
